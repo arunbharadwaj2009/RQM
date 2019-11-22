135adcb0e10203504d367530bebffd64998babe8
Dhaval <dudhat.dhaval.m@gmail.com>
2019-11-22 17:46:52 +0530

    Image show with modal

--- potholes/admin.py
+++ potholes/admin.py
@@ -3,7 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib.gis.admin import OSMGeoAdmin
 from .models import Pothole
+from django.utils.html import format_html
 
 @admin.register(Pothole)
 class PotholeAdmin(OSMGeoAdmin):
-    list_display = ('name', 'location')
+
+    def image_tag(self, obj):
+        return format_html('<img src="{}" width="50px" />'.format("/static/Gir_Arun.jpg"))
+
+    image_tag.short_description = 'Image' 
+
+    list_display = ('name', 'location', "image_tag")
--- potholes/models.py
+++ potholes/models.py
@@ -5,13 +5,6 @@ from django.contrib.gis.db import models
 
 class Pothole(models.Model):
     name = models.CharField(max_length=100)
-    location = models.PointField()
-    #img = models.ImageField()
+    location = models.PointField()    
     address = models.CharField(max_length=100)
     city = models.CharField(max_length=50)
-
-    def image_tag(self):
-        from django.utils.html import escape
-        return u'<img src="%s" />' % escape(<URL to the image>)
-    image_tag.short_description = 'Image'
-    image_tag.allow_tags = True
