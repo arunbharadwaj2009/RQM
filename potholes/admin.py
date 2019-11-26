b2b69a4df31672ba7e4add41fc08aa46bb151d48
Dhaval <dudhat.dhaval.m@gmail.com>
2019-11-26 12:51:12 +0530

    Open Image In New Tab

--- potholes/admin.py
+++ potholes/admin.py
@@ -5,15 +5,17 @@ from django.contrib.gis.admin import OSMGeoAdmin
 from .models import Pothole
 from django.utils.html import format_html
 
+
 @admin.register(Pothole)
 class PotholeAdmin(OSMGeoAdmin):
-    
+
     def image_tag(self, obj):
         html = ""
         for imageName in obj.imageNames.split("|"):
-            html += '<img src="{}" width="24px" />'.format("/static/"+ imageName)
-        return format_html(html)        
+            html += '<a href="{}" target="_blank"><img src="{}" width="24px" /></a>'.format(
+                "/static/" + imageName, "/static/" + imageName)
+        return format_html(html)
 
-    image_tag.short_description = 'Images' 
+    image_tag.short_description = 'Images'
 
     list_display = ('name', 'location', "image_tag")
